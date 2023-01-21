0c39e14f5c5796bd79705a2f5818d6b19e8e5c99
Nicole <nbain@purdue.edu>
2023-01-21 15:13:49 -0500

    ordered words from greatest to least

--- src/AnagramsBot.java
+++ src/AnagramsBot.java
@@ -8,15 +8,15 @@ public class AnagramsBot {
         Scanner s = new Scanner(System.in);
         String input;
         do {
-            System.out.println("Enter your 7 letters from your Anagrams game(1 string, no spaces):");
+            System.out.println("Enter your 6 letters from your Anagrams game(1 string, no spaces):");
             input = s.nextLine();
             input = input.toUpperCase();
-            if (input.length() != 7 || input.contains(" ")) {
+            if (input.length() != 6 || input.contains(" ")) {
                 System.out.println("Please enter valid input.");
             }
-        } while (input.length() != 7 || input.contains(" "));
-        String[] finalLetters = new String[7];
-        for (int i = 0; i < 7; i++) {
+        } while (input.length() != 6 || input.contains(" "));
+        String[] finalLetters = new String[6];
+        for (int i = 0; i < 6; i++) {
             finalLetters[i] = String.valueOf(input.charAt(i));
         }
         ArrayList<String> validWords = new ArrayList<>();
@@ -31,11 +31,11 @@ public class AnagramsBot {
         }
         ArrayList<String> lettersNew = new ArrayList<>();
         ArrayList<String> finalWords = new ArrayList<>();
-        //String[] letters = finalLetters;
+
         for (int p = 0; p < finalLetters.length; p++) {
             lettersNew.add(finalLetters[p]);
         }
-        // int count = 0;
+
         int count1 = 0;
         for (int i = 0; i < validWords.size(); i++) {
             for (int j = 0; j < validWords.get(i).length(); j++) {
@@ -58,8 +58,31 @@ public class AnagramsBot {
             }
             count1 = 0;
         }
-        for(int k = 0; k < finalWords.size(); k++) {
-           System.out.println(finalWords.get(k));
+        ArrayList<String> size3 = new ArrayList<>();
+        ArrayList<String> size4 = new ArrayList<>();
+        ArrayList<String> size5 = new ArrayList<>();
+        ArrayList<String> size6 = new ArrayList<>();
+        for(int x = 0; x < finalWords.size(); x++) {
+           if (finalWords.get(x).length() == 3) {
+               size3.add(finalWords.get(x));
+           } else if (finalWords.get(x).length() == 4) {
+               size4.add(finalWords.get(x));
+           } else if (finalWords.get(x).length() == 5) {
+               size5.add(finalWords.get(x));
+           } else {
+               size6.add(finalWords.get(x));
+           }
+        }
+        for (int z = 0; z < finalWords.size(); z++) {
+            if (z < size6.size()) {
+                System.out.println(size6.get(z));
+            } else if (z < size6.size() + size5.size()) {
+                System.out.println(size5.get(z - size6.size()));
+            } else if (z < size4.size() + size5.size() + size6.size()){
+                System.out.println(size4.get(z - size6.size() - size5.size()));
+            } else {
+                System.out.println(size3.get(z - size6.size() - size5.size() - size4.size()));
+            }
         }
     }
 }
