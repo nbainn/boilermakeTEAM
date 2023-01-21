2f96a93d4fcd4f58a0d7eaaea7dd691eb1c40eb5
Nicole <nbain@purdue.edu>
2023-01-21 13:29:09 -0500

    console stuff works

--- src/AnagramsBot.java
+++ src/AnagramsBot.java
@@ -10,6 +10,7 @@ public class AnagramsBot {
         do {
             System.out.println("Enter your 7 letters from your Anagrams game(1 string, no spaces):");
             input = s.nextLine();
+            input = input.toUpperCase();
             if (input.length() != 7 || input.contains(" ")) {
                 System.out.println("Please enter valid input.");
             }
@@ -33,7 +34,7 @@ public class AnagramsBot {
         int count = 0;
         for (int i = 0; i < validWords.size(); i++) {
             for (int j = 0; j < validWords.get(i).length(); j++) {
-                if (input.contains(validWords.get(i).substring(j, j + 1))) {
+                if (input.contains(validWords.get(i).substring(j, j+1))) {
                     count++;
                 }
             }
@@ -41,8 +42,8 @@ public class AnagramsBot {
                 finalWords.add(validWords.get(i));
             }
         }
-        for(int i = 0; i < finalWords.size(); i++) {
-            System.out.println(finalWords.get(i));
+        for(int k = 0; k < finalWords.size(); k++) {
+            System.out.println(finalWords.get(k));
         }
     }
 }
